c7114eb86bb7ccbb57efbe624fb00fef9124579d
nicolasmartinomaero <nicolasmartinomaero@gmail.com>
2022-08-09 13:52:43 -0300

    creando componentes

--- Proyecto_Martino/src/app/app.module.ts
+++ Proyecto_Martino/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoApComponent } from './componentes/logo-ap/logo-ap.component';
+import { RedSocialComponent } from './componentes/red-social/red-social.component';
+import { BanerComponent } from './componentes/baner/baner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    RedSocialComponent,
+    BanerComponent
   ],
   imports: [
     BrowserModule,
