fba364aaf59c23062b58070b63d9ce136301e290
nicolasmartinomaero <nicolasmartinomaero@gmail.com>
2022-08-10 13:04:44 -0300

    listos componentes de educacion y experiencia

--- Proyecto_Martino/src/app/app.module.ts
+++ Proyecto_Martino/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HeaderComponent } from './componentes/header/header.component';
 import { LogoApComponent } from './componentes/logo-ap/logo-ap.component';
 import { RedSocialComponent } from './componentes/red-social/red-social.component';
 import { BanerComponent } from './componentes/baner/baner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { BanerComponent } from './componentes/baner/baner.component';
     HeaderComponent,
     LogoApComponent,
     RedSocialComponent,
-    BanerComponent
+    BanerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
