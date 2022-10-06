f640ef10ba35b0ef5cbf45c35c17db91957299d1
Nicolas Martino Maero <95449968+nicolasmartinomaero@users.noreply.github.com>
2022-10-06 09:56:16 -0300

    .

--- Proyecto_Martino/src/app/app.module.ts
+++ Proyecto_Martino/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { LogoApComponent } from './componentes/logo-ap/logo-ap.component';
-import { RedSocialComponent } from './componentes/red-social/red-social.component';
 import { BanerComponent } from './componentes/baner/baner.component';
 import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
@@ -13,6 +12,7 @@ import { EducacionComponent } from './componentes/educacion/educacion.component'
 import { HysComponent } from './componentes/hys/hys.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { ProyectoComponent } from './componentes/proyecto/proyecto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 
 @NgModule({
@@ -20,13 +20,13 @@ import { ProyectoComponent } from './componentes/proyecto/proyecto.component';
     AppComponent,
     HeaderComponent,
     LogoApComponent,
-    RedSocialComponent,
     BanerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
     HysComponent,
-    ProyectoComponent
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
